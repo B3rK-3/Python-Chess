0faeb88f752fa8b97838428312895315ec5a305c
Artist76 <erenberkedogan@gmail.com>
2024-04-25 20:00:09 -0400

    Further Progress
    Implemented move functions. Move can be done both by dragging and clicking. Move log file is kept. Chess notation is implemented (Does not account for piece capture).

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -19,4 +19,24 @@ class GameState():
         self.white_turn = True
         # This will be the list that will keep track of the moves
         self.mLog = []
+    def makeMove(self, move):
+        self.board[move.startRow][move.startCol] = "__"
+        self.board[move.endRow][move.endCol] = move.pieceMoved
+        self.mLog.append(move)
+        self.white_turn = not self.white_turn
+class Move():
+    ranksToRows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+    rowsToRanks = {v:k for k, v in ranksToRows.items()}
+    filesToCols = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    colsToFiles = {v:k for k, v in filesToCols.items()}
+    def __init__(self, startSq, endSq, board):
+        self.startRow = startSq[0]
+        self.startCol = startSq[1]
+        self.endRow = endSq[0]
+        self.endCol = endSq[1]
+        self.pieceMoved = board[self.startRow][self.startCol]
+        self.pieceCaptured = board[self.endRow][self.endCol]
+
+    def getChessNotation(self):
+        return self.colsToFiles[self.startCol] + self.rowsToRanks[self.startRow] + self.colsToFiles[self.endCol] + self.rowsToRanks[self.endRow]
 
--- Chess/main.py
+++ Chess/main.py
@@ -2,9 +2,9 @@
 Main.py file is responsible for running the game engine. Will handle user input and GUI.
 """
 import pygame as p
-from Chess import ChessEngine
+from Python_Chess_Engine.Chess import ChessEngine
 
-WIDTH = HEIGHT = 800
+WIDTH = HEIGHT = 600
 SIZE = 8
 SQ_EACH_SIZE = HEIGHT // SIZE
 MAX_FPS = 15
@@ -16,7 +16,7 @@ def load_images():
     """
     p_names = ['wP', 'wR', 'wB', 'wN', 'wQ', 'wK', 'bP', 'bR', 'bB', 'bN', 'bQ', 'bK']
     for e in p_names:
-        IMAGES[e] = p.transform.scale(p.image.load('Chess/img/' + e + '.png'), (SQ_EACH_SIZE, SQ_EACH_SIZE))
+        IMAGES[e] = p.transform.scale(p.image.load('img/' + e + '.png'), (SQ_EACH_SIZE, SQ_EACH_SIZE))
 
 def main(): # Driver code for the main chess game
     p.init()
@@ -27,11 +27,41 @@ def main(): # Driver code for the main chess game
     FPS = MAX_FPS
     gs = ChessEngine.GameState()
     running = True
+    dragging = False
+    move = [] # two tuples [(7,7), (6,3)] will store where the user wants to move the piece to
+    mpx = 0
+    mpy = 0
     while running:
         for e in p.event.get():
             if e.type == p.QUIT:
                 running = False
-        draw_game_state(screen,gs)
+            elif e.type == p.MOUSEBUTTONDOWN:
+                mx, my = p.mouse.get_pos()
+                if mx < HEIGHT and my < WIDTH:
+                    mpx = mx//SQ_EACH_SIZE
+                    mpy = my//SQ_EACH_SIZE
+                    move.append((mpx, mpy))
+                    if len(move) == 2 and (move[0] == move[1]) or gs.board[move[0][1]][move[0][0]] == "__": # clears move if the tuples move[0] and move[1] are the same and if the last move was done by dragging also removes move if the first clik was void
+                        move.clear()
+                if len(move) == 2: # making sure there were two clicks before
+                    moveC = ChessEngine.Move((move[0][1], move[0][0]), (move[1][1], move[1][0]), gs.board)
+                    gs.makeMove(moveC)
+                    print(moveC.getChessNotation())
+                    move.clear()
+                dragging = True
+            elif e.type == p.MOUSEBUTTONUP:
+                mx, my = p.mouse.get_pos()
+                my = my//SQ_EACH_SIZE
+                mx = mx//SQ_EACH_SIZE
+                if mpx < SIZE and mpy < SIZE and mx < HEIGHT and my < WIDTH and gs.board[mpy][mpx] != "__" and (mx,my) != (mpx, mpy): #make sure that the dragged is not empty and within bounds
+                    #for the if condition the last condition makes sure that the we are not trying to drag it into the same place (it messes with the click move property)
+                    moveC = ChessEngine.Move((mpy, mpx), (my, mx), gs.board)
+                    gs.makeMove(moveC)
+                    mpx, mpy = 1000, 1000
+                    dragging = False
+                    move.clear()
+            p.display.flip()
+        draw_game_state(screen, gs)
         clock.tick(MAX_FPS)
         p.display.flip()
 
@@ -53,19 +83,14 @@ def draw_board(screen):
     :return:
     """
     white = True
-    x = y = 0
     for r in range(SIZE):
         for c in range(SIZE):
             if white:
-                p.draw.rect(screen, p.Color(227,193,111), (x, y, SQ_EACH_SIZE, SQ_EACH_SIZE))
-                white = False
+                p.draw.rect(screen, p.Color(227,193,111), (c*SQ_EACH_SIZE, r*SQ_EACH_SIZE, SQ_EACH_SIZE, SQ_EACH_SIZE))
             else:
-                p.draw.rect(screen, p.Color(184,139,74), (x, y, SQ_EACH_SIZE, SQ_EACH_SIZE))
-                white = True
-            x += SQ_EACH_SIZE
+                p.draw.rect(screen, p.Color(184,139,74), (c*SQ_EACH_SIZE, r*SQ_EACH_SIZE, SQ_EACH_SIZE, SQ_EACH_SIZE))
+            white = not white
         white = not white
-        y += SQ_EACH_SIZE
-        x = 0
 def draw_pieces(screen, board):
     """
     draw the pieces
@@ -73,8 +98,12 @@ def draw_pieces(screen, board):
     :param gs:
     :return:
     """
-    for each in screen:
-        print(1)
+    for r in range(SIZE):
+        for c in range(SIZE):
+            pc = board[r][c]
+            if pc != "__": #not empty
+                screen.blit(IMAGES[pc], p.Rect(c*SQ_EACH_SIZE, r*SQ_EACH_SIZE, SQ_EACH_SIZE, SQ_EACH_SIZE))
+
 
 if __name__ == '__main__':
     main()
